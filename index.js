3ea24f48d897a1966a75fade02b05abb3a31cf8c
VictorLCosta <victorlc2019@outlook.com>
2021-04-13 21:31:29 -0300

    Validations

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const flash = require('express-flash');
 
@@ -8,6 +9,8 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json);
 
+
+app.use(cookieParser("akaskd=kasdfoasf"))
 app.use(session({
     secret: 'ZWRpw6fDo28gZW0gY2/9tcHV0YWRvcmE=',
     cookie: {
@@ -17,14 +20,42 @@ app.use(session({
     saveUninitialized: true,
     resave: true
 }));
-
 app.use(flash());
 
-app.get('/', (req, res) => 
+
+app.get('/', (req, res) =>
 {
-    console.log("Ta bala!")
+    var emailError = req.flash("emailError");
+
+    res.render('index', {email: emailError});
 });
 
+app.post('/form', (req, res) => 
+{
+    var {email, name, points} = req.body;
+
+    var emailError;
+    var nameError;
+    var pointsError;
+
+    if(email == undefined || email == "") 
+    {
+        //result
+    }
+    else if(points == undefined || points < 20)
+    {
+        //result
+    }
+    else if(name == undefined || name == "")
+    {
+        //result
+    }
+
+    if(name.length < 4)
+    {
+        //result
+    }
+});
 
 app.listen(8080, () => {
     console.log("Site rodando");
